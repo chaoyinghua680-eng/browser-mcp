c33e6e2f60921e3e9e25fc5b7519d24a61b2283b
石东stone <14516010+shidong-stone@user.noreply.gitee.com>
2026-03-31 19:38:47 +0800

    增加了windows部署的ps1文件

---
+++ native-host/start.cmd
@@ -0,0 +1,2 @@
+@echo off
+"D:\application\nodejs\node.exe" "D:\VS_Projects\browser-mcp\native-host\dist\index.js"
--- native-host/start.sh
+++ native-host/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec /opt/homebrew/bin/node "/Users/jjchao/browser-mcp/native-host/dist/index.js"
+exec node "/d/VS_Projects/browser-mcp/native-host/dist/index.js"
